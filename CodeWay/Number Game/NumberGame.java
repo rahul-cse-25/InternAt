644a701de83f0f51a9b13f44a3912eca3cb070f4
Rahul Prajapati <152918567+rahul-cse-25@users.noreply.github.com>
2024-01-30 22:17:27 +0530

    Update NumberGame.java

--- CodeWay/Number Game/NumberGame.java
+++ CodeWay/Number Game/NumberGame.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
+// check
+
 public class NumberGame {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);

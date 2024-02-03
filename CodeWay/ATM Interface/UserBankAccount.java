dd6d77d378e9f1c4f3299d8e893af6b1954a2923
Rahul Prajapati <152918567+rahul-cse-25@users.noreply.github.com>
2024-02-03 10:44:50 +0530

    Update UserBankAccount.java

--- CodeWay/ATM Interface/UserBankAccount.java
+++ CodeWay/ATM Interface/UserBankAccount.java
@@ -1,8 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserBankAccount {
 	private double balance;
 	private int atmCardNum;
 	private int atmPinNum;
+	private List<Transaction> statement = new ArrayList<>();
+
+	public class Transaction {
+		private double amount;
+		private double balance;
+		private String description;
+
+		public Transaction(double amount, double balance, String description) {
+			this.amount = amount;
+			this.balance = balance;
+			this.description = description;
+		}
+
+		public double getAmount() {
+			return amount;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public double getBalance() {
+			return balance;
+		}
+	}
+	
+	public void addTransaction(double amount, String description) {
+		statement.add(new Transaction(amount, balance, description));
+	}
 
 	public UserBankAccount(double balance, int atmCardNum, int atmPinNum) {
 		this.balance = balance;
@@ -29,6 +60,10 @@ public class UserBankAccount {
 	@Override
 	public String toString() {
 		return "UserBankAccount [balance=" + balance + ", atmCardNum=" + atmCardNum + ", atmPinNum=" + atmPinNum + "]";
+	}
+
+	public List<UserBankAccount.Transaction> getStatement() {
+		return statement;
 	}
 
 }
